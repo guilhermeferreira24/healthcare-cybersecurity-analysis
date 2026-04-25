ddde20f0d8a65d409e798c2d578e2b836583920a
Guilherme Ferreira <guilherme20ferreira03@gmail.com>
2026-04-25 22:11:35 +0100

    Add SQL query for severity distribution of CVEs

---
+++ 01_severity_distribution.sql
@@ -0,0 +1,7 @@
+SELECT
+  Severity,
+  COUNT(*) AS total_cves,
+  ROUND(COUNT(*) * 100.0 / SUM(COUNT(*)) OVER (), 2) AS pct
+FROM `healthcare-cybersecurity.healthcare_cves.cve_records`
+GROUP BY Severity
+ORDER BY total_cves DESC;
