cf140324979eeb787dda0a168a78197131eedef1
Guilherme Ferreira <guilherme20ferreira03@gmail.com>
2026-04-25 22:21:37 +0100

    Update 01_severity_distribution.sql

--- 01_severity_distribution.sql
+++ 01_severity_distribution.sql
@@ -6,10 +6,3 @@ FROM `healthcare-cybersecurity.healthcare_cves.cve_records`
 GROUP BY Severity
 ORDER BY total_cves DESC;
 
-
-| Severity | Total CVEs | % |
-|----------|-----------|---|
-| MEDIUM | 720 | 48.10% |
-| HIGH | 492 | 32.87% |
-| CRITICAL | 151 | 10.09% |
-| LOW | 134 | 8.95% |
